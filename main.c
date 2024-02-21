313366f6a898ce0243fa28001f86ec35e37d1a71
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-02-21 19:40:47 +0000

    Got a working socket

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(void) {
-    printf("Hello World");
-    printf("\nServer Up\n");
-    return 0;
+  printf("Hello World");
+  printf("\nServer Up\n");
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(sock, 32) < 0) {
+    perror("Error in Listen");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("%d\n", sock);
+
+  return 0;
 }
